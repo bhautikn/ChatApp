52f8a934c65989a8be4620f93bf9e518f1c955a1
Bhautik Navdariya <navadariyabhautikm@gmail.com>
2023-12-23 04:33:13 -0500

    create angular app

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const crypto = require('crypto');
 const app = express();
 
-// app.get('/', (req, res)=>{
-//     res.send("Hello World");
-// })
+app.get('/get-id', (req, res)=>{
+    id = crypto.randomBytes(32).toString('hex');
+    res.send({code:id})
+})
 
 app.listen(3000, ()=>{
     console.log("App listing on", 3000);
